35c4aa394321fe81b66e7c87eb53dcc44f7d3641
Anne <annecoleen.s@gmail.com>
2016-05-12 16:16:53 +0200

    Création d'une classe "camp", avec methode tourGestion

--- Gestion/main.cpp
+++ Gestion/main.cpp
@@ -1,5 +1,6 @@
 #include "carte.h"
 #include "unite.h"
+#include "camp.h"
 
 
 int main() {
@@ -71,68 +72,15 @@ int main() {
     boutonAction.affiche();
     boutonInventaire.affiche();
 
+    Camp allie(unites);
+
     // Deplacement des unites
     bool save = true;
-    while (save) {
-        int x = -1, y = -1, x1 = -1, y1 = -1;
-        survole(x1, y1);
-        clic(x, y, carte);
-        int u = 0;
-
-        // Le bouton sauvegarde estil active ?
-        if (boutonFinTour.boutonActive(x, y)) {
-            finJournee(unites);
-        }
-
-        // Le bouton FinTour est-il active ?
-        if (boutonSauvegarde.boutonActive(x, y)) {
-            sauvegarde(unites);
-            save = false;
-        }
-        // Vient-on de cliquer sur une unite ?
-        if (numeroCase(x, y) != -1 && carte[numeroCase(x, y)].getOccupe()) {
-            while (unites[u]->getCase() != numeroCase(x, y)) {
-                u += 1;
-            }
-
-            float dep = unites[u]->getDep();
-
-            unites[u]->afficheCaseDisponibleOnOff(carte, true, dep, 0);
-            boutonAction.affiche();
-            boutonInventaire.affiche();
-
-            clic(x, y, carte);
-
-            // A modifier
-            for (int i = 0; i < NbCase; ++i) {
-                for (int j = 0; j < NbCase; ++j) {
-                    carte[j * NbCase + i].affiche();
-                }
-            }
-            // Bouton inventaire
-            if (boutonInventaire.boutonActive(x, y)) {
-                unites[u]->ouvreInventaire();
-                // unites[u]->equipe(1);
-                // unites[u]->ouvreInventaire();
-                // Reaffichage de la carte
-                for (int i = 0; i < NbCase; i++) {
-                    for (int j = 0; j < NbCase; j++) {
-                        carte[NbCase * j + i].affiche();
-                    }
-                }
-            }
-            else {
-                unites[u]->deplacement(carte, false, x, y);
-            }
-
-            boutonSauvegarde.affiche();
-            boutonFinTour.affiche();
-
-            unites[u]->afficheCaseDisponibleOnOff(carte, false, dep, 0);
-            boutonAction.affiche();
-            boutonInventaire.affiche();
-        }
+    while(save) {
+        allie.tourGestion(carte, unites, boutonFinTour, boutonSauvegarde, boutonAction, boutonInventaire, save);
+        finJournee(unites);
     }
+
 
 
     // Destruction des unités
